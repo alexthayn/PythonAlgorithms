01e9b6b2248c7eb9089ca6319fd21fecf3a57c36
Alex Thayn <alexat789@gmail.com>
2019-02-15 11:11:49 -0700

    Added lab4 file

---
+++ Lab4SCC/Lab4SCC.py
@@ -0,0 +1,10 @@
+# Lab 4 Strongly Connected Components
+# Alex Thayn 
+
+#Take an adjacency list representation of a graph and returns a reversed graph
+
+#Take an adjacency list representation of a graph and performs a depth-first search (in linear time)
+
+#Test reversed graph function
+#Test depth-first search function
+
