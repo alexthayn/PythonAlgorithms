8d1469ecf120da99a4f6d79532eae05537769bca
alexthayn <alexat789@gmail.com>
2019-03-06 18:32:06 -0700

    Added more test cases for the unbounded knapsack problme

--- Lab7/Lab7_DynamicProgramming.py
+++ Lab7/Lab7_DynamicProgramming.py
@@ -2,11 +2,11 @@
 from collections import namedtuple
 
 # With repeated items
-def knapval_rep(capacity, items):
+def knapsack_unbounded(capacity, items):
     K = [0]*(capacity+1)
 
     for wt in range(1,len(K)):
-        K[wt] = max([(K[wt-item.weight] + item.value) #list comprehension syntax
+        K[wt] = max([(K[wt-item.weight] + item.value) # list comprehension syntax is pretty cool
         for item in items if item.weight <= wt]+[0])
     return K[capacity]
 
@@ -20,13 +20,20 @@ if __name__ == "__main__":
         Item(4,16),
         Item(2,9)
     ]
-    print("For the knapsack problem with repets the max value is: %d"%knapval_rep(10,loot))
+    print("For the knapsack problem with repeats the max value is: %d"%knapsack_unbounded(500,loot))
 
-def test_knapval_rep():
+def test_knapsack_unbounded():
     loot = [
         Item(6,30),
         Item(3,14),
         Item(4,16),
         Item(2,9)
     ]
-    assert knapval_rep(10,loot) == 48
+    assert knapsack_unbounded(0,loot) == 0
+    assert knapsack_unbounded(1,loot) == 0
+    assert knapsack_unbounded(2,loot) == 9
+    assert knapsack_unbounded(3,loot) == 14
+    assert knapsack_unbounded(4,loot) == 18
+    assert knapsack_unbounded(10,loot) == 48
+    assert knapsack_unbounded(50, loot) == 249
+    assert knapsack_unbounded(1000000, loot) == 4999998 # 166666(Item1) + 2(Item4) = 166666*30 + 2*9
