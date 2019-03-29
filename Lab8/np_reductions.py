45c2fad01cd6d4c5f44c1a9cb466ba90c7e6796a
Alex Thayn1 <alex.thayn1@ad.snow.edu>
2019-03-29 11:06:46 -0600

    Added Lab 9: NP Reductions

---
+++ Lab8/np_reductions.py
@@ -0,0 +1,11 @@
+# Alex Thayn
+# Lab 9: NP Reductions
+
+# Implement and test the reduction from SAT to ILP using PULP
+
+# Example input: ilp_sat(['a,b,c', '-a,b,-c', '-d,-b', '-a,c', '-b,a'])
+
+def ilp_sat(sat_formula):
+    pass
+
+    
